c171ea75ba222b8df356c0dddcd9d0d42870d9d8
notmalicik <98515894+notmalicik@users.noreply.github.com>
2022-04-15 14:05:08 +0300

    xampp

--- javascript/top.js
+++ javascript/top.js
@@ -1,13 +1,23 @@
 mybutton = document.getElementById("myBtn");
+fixedheader = document.getElementById("fixed-header");
+fixedheader.style.position = 'absolute';
+fixedheader.style.marginTop = "335px";
 
 
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
+    }
+    if (document.body.scrollTop > 335 || document.documentElement.scrollTop > 335) {
+        fixedheader.style.position = 'fixed';
+        fixedheader.style.marginTop = "0px";
+    } else {
+        fixedheader.style.position = 'absolute';
+        fixedheader.style.marginTop = "335px";
     }
 }
 
